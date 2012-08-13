13fe51bbefa076decd8b0557d00198646dbc07c1
Ian Wojtowicz <i@woj.com>
2012-08-13 10:23:22 -0700

    Added basic reciprocal regexp editing. There are some noticeable bugs, such as the fact that each edit of the regexp refreshes the whole document, instead of just the elements that need highlighting. Also, highlights that span across unspannable tags does not yet work.

--- Source Code/glean.js
+++ Source Code/glean.js
@@ -5,8 +5,7 @@ var baseURL = "<%= baseURL %>";
 window.Glean.bootstrapper.loadScripts([
     baseURL + "/jquery.min.js",
     baseURL + "/underscore-min.js", 
-    baseURL + "/backbone-min.js",
-    baseURL + "/xregexp-min.js"
+    baseURL + "/backbone-min.js"
   ], initGlean );
 
 
@@ -55,46 +54,38 @@ function checkSelection() {
 
 
 function matchWebpage() {
-  var html = document.documentElement.outerHTML;
-  XRegExp.forEach(html, new RegExp(trimSlashes($("#glean-regex").text())), function(match, i) {
-    
-    var gleanStartIndex = html.indexOf('<div id="glean"')
-    var gleanEndIndex = gleanStartIndex + $("#glean")[0].outerHTML.length
-    
-    if (match.index < gleanStartIndex || match.index > gleanEndIndex) {
-      if (DEBUG) console.log("match.index: " + match.index)
-      if (DEBUG) console.log("gleanStartIndex: " + gleanStartIndex)
-      if (DEBUG) console.log("gleanEndIndex: " + gleanEndIndex)
-      //alert(match.index)
-      setSelection(match.index, match.index + match[0].length)
+  var html = removeHighlights($("body").html());
+  html = html.replace(/<textarea id="glean-regex">.*?<\/textarea>/, "<textarea id=\"glean-regex\">" + $("#glean-regex").val() + "</textarea>");
+  var gleanStartIndex = html.indexOf('<div id="glean"');
+  var gleanEndIndex = gleanStartIndex + $("#glean")[0].outerHTML.length;
+  var regexp = new RegExp(trimSlashes($("#glean-regex").val()), "g");
+  var matchRanges = [];
+  
+  while(result = regexp.exec(html)) {
+    if (result.index < gleanStartIndex || result.index > gleanEndIndex) {
+      matchRanges.push([result.index, result.index + result[0].length]);
     }
-       // http://stackoverflow.com/questions/4183401/can-you-set-and-or-change-the-users-text-selection-in-javascript
-       // http://xregexp.com/api/
-  })
+  }
+  
+  $("body").html(addHighlights(html, matchRanges));
+  registerEvents(); // Event handlers need to be rebuilt after the new html is loaded
 }
 
 
 function setSelection(startIndex, endIndex) {
-  // find element in startIndex
-  // determine offset
-  // same for end index
-  // setselection, appending the selection range to previous select ranges, if any exist
-  
+  var selection = window.getSelection();
   var range = document.createRange();
-  range.setStart(document.getElementByOffset(startIndex), startIndex);
-  range.setEnd(document.getElementByOffset(endIndex), endIndex);
+  var html = $("body").html();
+  var startElement = document.getElementByOffset(startIndex);
+  var endElement = document.getElementByOffset(endIndex);
   
-    // if (window.getSelection && document.createRange) {
-    //     var sel = window.getSelection();
-    //     var range = document.createRange();
-    //     range.selectNodeContents(el);
-    //     sel.removeAllRanges();
-    //     sel.addRange(range);
-    // } else if (document.selection && document.body.createTextRange) {
-    //     var textRange = document.body.createTextRange();
-    //     textRange.moveToElementText(el);
-    //     textRange.select();
-    // }
+  range.setStart(startElement, startIndex - html.indexOf(startElement.outerHTML));
+  range.setEnd(endElement, endElement - html.indexOf(endElement.outerHTML));
+  
+  selection.addRange(range);
+  
+  selectElement(startElement, endElement);
+  
 }
 
 
@@ -111,6 +102,74 @@ function getSelectionText() {
 
 
 
+function removeHighlights(html) {
+  return html.replace(/<span class="glean-highlight">(.+?)<\/span>/g, "$1");
+}
+
+
+
+// Assume that the ranges are in sequential order and non-overlapping.
+//
+// This is all fucked up and needs to be rewritten.
+//
+function addHighlights(html, ranges) {
+  var openTag = "<span class=\"glean-highlight\">";
+  var closeTag = "</span>";
+  var tagLength = (openTag.length + closeTag.length);
+  var offset = 0;
+  
+  for (var j = 0; j < ranges.length; j++) {
+    var middleSegment = html.slice(ranges[j][0], ranges[j][1]);
+    
+    // The length of the HTML file changes as highlights are added, so we need this offset.
+    
+    var middleSegmentOffset = 0;
+    if (isUnspannable(middleSegment)) {
+      var segmentArray = findSpannableSegments(middleSegment);
+      middleSegment = addHighlights(middleSegment, segmentArray);
+      middleSegmentOffset = tagLength * segmentArray.length;
+    }
+
+    html = html.slice(0, ranges[j][0] + offset) + openTag + middleSegment + closeTag + html.slice(ranges[j][1] + offset + middleSegmentOffset);
+    offset += tagLength + middleSegmentOffset;
+  }
+  return html;
+}
+
+
+
+function findSpannableSegments(html) {
+  var spannableSegments = [];
+  var lastTagEndIndex = 0;
+  var segmentCount = 0;
+  var regexp = new RegExp(unspannableTagsRegExp(), "ig");
+  
+  while (result = regexp.exec(html)) {
+    spannableSegments[segmentCount] = [lastTagEndIndex, result.index];
+    lastTagEndIndex = result.index + result[0].length;
+    segmentCount++;
+  }
+  if (lastTagEndIndex != html.length) {
+    spannableSegments[spannableSegments.length] = [lastTagEndIndex, spannableSegments.length];
+  }
+  return spannableSegments;
+}
+
+
+
+function isUnspannable(middleSegment) {
+  return new RegExp(unspannableTagsRegExp(), "i").test(middleSegment);
+}
+
+
+function unspannableTagsRegExp() {
+  var unspannableTags = ["<p>","<div>","<table>","<td>","<tr>"];
+  $.each(unspannableTags, function(i, tag) {
+    unspannableTags[i] = tag.replace(/</g, "<\s?\\/?\s?");
+    unspannableTags[i] = tag.replace(/>/g, ".*?>");
+  })
+  return "(" + unspannableTags.join("|") + ")";
+}
 
 function getSelectionHTML() {
     if (document.selection && document.selection.createRange) return (document.selection.createRange()).htmlText;
@@ -159,13 +218,21 @@ function registerEvents() {
       });
   });
   
-  $("#glean-regex").blur(function () {
-    matchWebpage();
+  $("#glean-regex").keyup(function (event) {
+    var ignoreKeys = [37, 38, 39, 40];
+    if (ignoreKeys.indexOf(event.which) == -1)
+      matchWebpage();
+  });
+  
+  $(document).mousedown(function(event) {
+    if (!document.getElementById("glean").contains(event.target))
+      $("body").html(removeHighlights($("body").html()));
   });
   
   $(document).mouseup(function(event) {
     if (!$("#glean").has(event.target).length) {
       checkSelection();
+      matchWebpage();
     }
   });
 }
@@ -218,6 +285,24 @@ function trimOuterTags(s) {
   return s;
 }
 
+function selectElement(element, element2) {
+    if (window.getSelection) {
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        var range = document.createRange();
+        range.selectNodeContents(element);
+        range.setStart(element, 5);
+        range.setEnd(element2, 5);
+        
+        sel.addRange(range);
+    } else if (document.selection) {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(element);
+        textRange.select();
+    }
+}
+
+
 
 document.getElementByOffset = function(targetOffset) {
   // traverse DOM tree
@@ -235,4 +320,8 @@ document.getElementByOffset = function(targetOffset) {
   return searchDOM(document.all[0], 0, targetOffset)
 }
 
+String.prototype.regexIndexOf = function(regex, startpos) {
+    var indexOf = this.substring(startpos || 0).search(regex);
+    return (indexOf >= 0) ? (indexOf + (startpos || 0)) : indexOf;
+}
 
